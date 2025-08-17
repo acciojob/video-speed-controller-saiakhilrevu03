b96c897760945f6ba61ac671373bcd0c8a2f3534
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-08-18 00:00:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const inputs = document.querySelectorAll('.controls input');
+const speed = document.querySelector('.speed');
+const bar = speed.querySelector('.speed-bar');
+const video = document.querySelector('video');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function handleMove(e) {
+  const y = e.pageY - speed.offsetTop;         // mouse position inside scrubber
+  const percent = y / speed.offsetHeight;      // percentage along scrubber
+  const min = 0.4;                             // minimum speed
+  const max = 4;                               // maximum speed
+  const playbackRate = percent * (max - min) + min;
+  
+  bar.style.height = `${Math.round(percent * 100)}%`;
+  bar.textContent = playbackRate.toFixed(2) + '×';
+  
+  video.playbackRate = playbackRate;
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+speed.addEventListener('mousemove', handleMove);
